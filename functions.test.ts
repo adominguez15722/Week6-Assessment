fdefec0ba04e5707d05eb334af8a0c27107132aa
Anthony <adominguez15722@gmail.com>
2022-02-11 10:39:10 -0600

    Part 3 is done

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,17 @@
 const {shuffleArray} = require('./utils')
 
 describe('shuffleArray should', () => {
-    // CODE HERE
+    test('return not a number', () => {
+        expect(shuffleArray([1, 2, 3])).toBeNaN()
+        
+    }),
+
+    
+    test('not return the same input in the same order', () => {
+        let newArr = [1, 2, 3]
+        
+        expect(shuffleArray([1, 2, 3])).not.toBe(newArr)
+    })
+    
 })
+// expect((shuffleArray([1, 2, 3].length).toBe(2))}
