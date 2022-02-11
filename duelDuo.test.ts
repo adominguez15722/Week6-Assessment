1a24c4898da19da63d35ff7abd425211e4337f9d
Anthony <adominguez15722@gmail.com>
2022-02-11 10:02:59 -0600

    Part 2 is complete

---
+++ duelDuo.test.ts
@@ -0,0 +1,20 @@
+
+import { Builder, Capabilities, By } from "selenium-webdriver"
+// the above line is the same as const {} = require('')
+require('chromedriver')
+
+const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
+
+beforeEach(async () => {
+    driver.get('http://localhost:3000/')
+})
+
+afterAll(async () => {
+    driver.quit()
+})
+
+test('Title shows up when page loads', async () => {
+    const title = await driver.findElement(By.id('title'))
+    const displayed = await title.isDisplayed()
+    expect(displayed).toBe(true)
+})
